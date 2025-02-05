c43772061e6125a6dae0d0a052bcd80f8a45c709
Customable1011 <dixonethan04@gmail.com>
2025-02-05 18:36:36 +0000

    Quartz sync: Feb 5, 2025, 6:36 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
         repelForce: 0.5, // how much nodes should repel each other
         centerForce: 0.3, // how much force to use when trying to center the nodes
         linkDistance: 30, // how long should the links be by default?
-        fontSize: 0.6, // what size should the node labels be?
+        fontSize: 0.5, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
         showTags: true, // whether to show tags in the graph
@@ -53,7 +53,7 @@ export const defaultContentPageLayout: PageLayout = {
         repelForce: 10.0,
         centerForce: 0.1,
         linkDistance: 250,
-        fontSize: 0.3,
+        fontSize: 0.4,
         opacityScale: 1,
         removeTags: [], // what tags to remove from the graph
         showTags: true, // whether to show tags in the graph
